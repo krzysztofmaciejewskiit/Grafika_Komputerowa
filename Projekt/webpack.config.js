efb9c35f9df1d6bd19ce819e64b2882a605bcf8f
Krzysztof Maciejewski <krzysztofmaciejewski01@wp.pl>
2024-04-23 14:27:54 +0200

    ppp

---
+++ Projekt/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const glob = require('glob');
+
+module.exports = {
+  entry: {'app' : glob.sync('./src/**/*.js*')},
+  
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+};
